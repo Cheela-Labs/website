771ea2720cea9eaca6c077064ed8af1fb0469df4
Viren Tanti <virentanti16@gmail.com>
2026-07-19 17:13:58 +0530

    updated domains

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -4,7 +4,7 @@ import { getSiteOrigin } from "@/lib/seo";
 export default function sitemap(): MetadataRoute.Sitemap {
   return [
     {
-      url: "https://www.cheela.virentanti.in/sitemap.xml",
+      url: "https://www.cheelalabs.com/sitemap.xml",
       lastModified: new Date(),
       changeFrequency: "weekly",
       priority: 1,
